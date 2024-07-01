25632cc588ba977e547f9e7257958b4c1075f46d
Mike Cecconello <mikececco2000@gmail.com>
2024-07-01 22:19:46 +0200

    update vite config

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      ".prisma/client/index-browser": "./node_modules/.prisma/client/index-browser.js"
+    }
+  }
 })
